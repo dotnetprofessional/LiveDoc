357be0017aa6fce8b47eed8890852e974e14e189
Garry Mc <github@dotnetprofessional.com>
2017-09-27 23:18:22 -0700

    move definitions to globals

---
+++ packages/livedoc-mocha/_src/app/globals.ts
@@ -0,0 +1,61 @@
+
+
+declare var feature: Mocha.IContextDefinition;
+declare var background: Mocha.IContextDefinition;
+declare var scenario: Mocha.IContextDefinition;
+declare var scenarioOutline: Mocha.IContextDefinition;
+declare var given: Mocha.ITestDefinition;
+declare var when: Mocha.ITestDefinition;
+declare var then: Mocha.ITestDefinition;
+declare var and: Mocha.ITestDefinition;
+declare var but: Mocha.ITestDefinition;
+
+declare var afterBackground: (fn) => void;
+
+declare var featureContext: FeatureContext;
+declare var scenarioContext: ScenarioContext;
+declare var stepContext: StepContext;
+declare var backgroundContext: BackgroundContext;
+declare var scenarioOutlineContext: ScenarioOutlineContext;
+
+/**
+ * Represents a row in a data table as a keyed object
+ * 
+ * @interface DataTableRow
+ */
+declare interface DataTableRow {
+    [prop: string]: any;
+}
+
+declare interface String {
+    startsWith(searchString: string, position?: number);
+    endsWith(searchString: string, position?: number);
+    repeat(times: number);
+}
+
+// Polyfils
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+    };
+}
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function (searchString, position) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.lastIndexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
+}
+
+
+// initialize context variables
+featureContext = undefined;
+scenarioContext = undefined;
+stepContext = undefined;
+backgroundContext = undefined;
+scenarioOutlineContext = undefined;
--- packages/livedoc-mocha/_src/app/livedoc.ts
+++ packages/livedoc-mocha/_src/app/livedoc.ts
@@ -4,7 +4,6 @@ var moment = require("moment");
 */
 class LiveDocDescribe {
     protected _parser = new Parser();
-
     public id: number;
     public title: string;
     public rawDescription: string;
@@ -585,15 +584,6 @@ class Table {
     public dataTable: DataTableRow[] = [];
 }
 
-/**
- * Represents a row in a data table as a keyed object
- * 
- * @interface DataTableRow
- */
-declare interface DataTableRow {
-    [prop: string]: any;
-}
-
 class FeatureContext {
     filename: string;
     title: string;
@@ -679,56 +669,6 @@ class StepContext {
         return list;
     }
 }
-
-declare var feature: Mocha.IContextDefinition;
-declare var background: Mocha.IContextDefinition;
-declare var scenario: Mocha.IContextDefinition;
-declare var scenarioOutline: Mocha.IContextDefinition;
-declare var given: Mocha.ITestDefinition;
-declare var when: Mocha.ITestDefinition;
-declare var then: Mocha.ITestDefinition;
-declare var and: Mocha.ITestDefinition;
-declare var but: Mocha.ITestDefinition;
-
-declare var featureContext: FeatureContext;
-declare var scenarioContext: ScenarioContext;
-declare var stepContext: StepContext;
-declare var backgroundContext: BackgroundContext;
-declare var scenarioOutlineContext: ScenarioOutlineContext;
-
-declare var afterBackground: (fn) => void;
-
-declare interface String {
-    startsWith(searchString: string, position?: number);
-    endsWith(searchString: string, position?: number);
-    repeat(times: number);
-}
-
-// Polyfils
-if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function (searchString, position) {
-        position = position || 0;
-        return this.substr(position, searchString.length) === searchString;
-    };
-}
-if (!String.prototype.endsWith) {
-    String.prototype.endsWith = function (searchString, position) {
-        var subjectString = this.toString();
-        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
-            position = subjectString.length;
-        }
-        position -= searchString.length;
-        var lastIndex = subjectString.lastIndexOf(searchString, position);
-        return lastIndex !== -1 && lastIndex === position;
-    };
-}
-
-// initialize context variables
-featureContext = undefined;
-scenarioContext = undefined;
-stepContext = undefined;
-backgroundContext = undefined;
-scenarioOutlineContext = undefined;
 
 /** @internal */
 var _mocha = require('mocha'),
