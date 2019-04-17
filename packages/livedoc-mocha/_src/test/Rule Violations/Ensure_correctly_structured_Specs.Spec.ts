264306772fd69d0e0356bf7e8b029bee05b7b09e
Garry Mc <github@dotnetprofessional.com>
2019-04-16 21:47:58 -0700

    enabling addition examples

--- packages/livedoc-mocha/_src/test/Rule Violations/Ensure_correctly_structured_Specs.Spec.ts
+++ packages/livedoc-mocha/_src/test/Rule Violations/Ensure_correctly_structured_Specs.Spec.ts
@@ -3,53 +3,53 @@ import { ParserException } from "../../app/model";
 require('chai').should();
 
 
-feature(`Ensure correctly structured Specs`, () => {
-    let parseException: ParserException;
-    let outlineGiven: StepContext;
+feature(`Ensure correctly structured Specs
+    `, () => {
+        let parseException: ParserException;
+        let outlineGiven: StepContext;
 
-    scenarioOutline(`Invalid top level keywords
+        scenarioOutline(`Invalid top level keywords
 
         Examples:
         | keyword         | error message                                                                            |
         | scenario        | Scenario must be within a feature.                                                       |
         | scenarioOutline | Scenario Outline must be within a feature.                                               |
         | background      | Background must be within a feature.                                                     |
-      //| given           | Invalid Gherkin, Given can only appear within a Background, Scenario or Scenario Outline |
-      //| when            | Invalid Gherkin, When can only appear within a Background, Scenario or Scenario Outline  |
-      //| then            | Invalid Gherkin, Then can only appear within a Background, Scenario or Scenario Outline  |
-      //| and             | Invalid Gherkin, and can only appear within a Background, Scenario or Scenario Outline   |
-      //| but             | Invalid Gherkin, but can only appear within a Background, Scenario or Scenario Outline   |
+        | given           | Invalid Gherkin, Given can only appear within a Background, Scenario or Scenario Outline |
+        | when            | Invalid Gherkin, When can only appear within a Background, Scenario or Scenario Outline  |
+        | then            | Invalid Gherkin, Then can only appear within a Background, Scenario or Scenario Outline  |
+        | and             | Invalid Gherkin, and can only appear within a Background, Scenario or Scenario Outline   |
+        | but             | Invalid Gherkin, but can only appear within a Background, Scenario or Scenario Outline   |
 
-      # currently treating GWT as BDD, need to make code more strict before enabling
         `, () => {
-            given(`the following feature file
+                given(`the following feature file
                 """
                 <keyword>('Defining a <keyword> without a feature', () => {
                     given('a sample given', () => { });
                 });
                 """        
             `, () => {
-                    outlineGiven = stepContext;
+                        outlineGiven = stepContext;
+                    });
+
+                when(`executing feature`, async () => {
+                    try {
+                        await LiveDoc.executeDynamicTestAsync(outlineGiven.docString);
+                    }
+                    catch (e) {
+                        parseException = e;
+                    }
                 });
 
-            when(`executing feature`, async () => {
-                try {
-                    await LiveDoc.executeDynamicTestAsync(outlineGiven.docString);
-                }
-                catch (e) {
-                    parseException = e;
-                }
+                then(`a parseException with the following description is thrown 
+                """
+                <error message>
+                """`, () => {
+                        stepContext.docString.should.eql(parseException.description);
+                    });
             });
 
-            then(`a parseException with the following description is thrown 
-            """
-            <error message>
-            """`, () => {
-                    stepContext.docString.should.eql(parseException.description);
-                });
-        });
-
-    scenarioOutline(`Invalid Feature children
+        scenarioOutline(`Invalid Feature children
         Examples:
         | step  | display name |
         | given | Given        |
@@ -58,43 +58,43 @@ feature(`Ensure correctly structured Specs`, () => {
         | and   | and          |
         | but   | but          |
         `, () => {
-            let featureGherkin: string;
+                let featureGherkin: string;
 
-            given(`the following feature
+                given(`the following feature
             """
             feature("Ensure bad Gherkin is not allowed", () => {
                 <step>("a <step> step definition is added to a feature", () => { });
             });            
             """`, () => {
-                    featureGherkin = stepContext.docString;
+                        featureGherkin = stepContext.docString;
+                    });
+
+                when(`executing feature`, async () => {
+                    try {
+                        await LiveDoc.executeDynamicTestAsync(featureGherkin);
+                    }
+                    catch (e) {
+                        parseException = e;
+                    }
                 });
 
-            when(`executing feature`, async () => {
-                try {
-                    await LiveDoc.executeDynamicTestAsync(featureGherkin);
-                }
-                catch (e) {
-                    parseException = e;
-                }
-            });
-
-            then(`a parseException with the following description is thrown 
+                then(`a parseException with the following description is thrown 
             """
             Invalid Gherkin, <display name> can only appear within a Background, Scenario or Scenario Outline
             """`, () => {
-                    stepContext.docString.should.eql(parseException.description);
-                });
-        });
+                        stepContext.docString.should.eql(parseException.description);
+                    });
+            });
 
-    scenarioOutline(`Invalid Background children
+        scenarioOutline(`Invalid Background children
         Examples:
         | step | display name |
         | when | When         |
         | then | Then         |
         `, () => {
-            let featureGherkin: string;
+                let featureGherkin: string;
 
-            given(`the following feature
+                given(`the following feature
             """
             feature("Ensure bad Gherkin is not allowed", () => {
                 background("", ()=> {
@@ -102,69 +102,69 @@ feature(`Ensure correctly structured Specs`, () => {
                 });
             });            
             """`, () => {
-                    featureGherkin = stepContext.docString;
+                        featureGherkin = stepContext.docString;
+                    });
+
+                when(`executing feature`, async () => {
+                    try {
+                        await LiveDoc.executeDynamicTestAsync(featureGherkin);
+                    }
+                    catch (e) {
+                        parseException = e;
+                    }
                 });
 
-            when(`executing feature`, async () => {
-                try {
-                    await LiveDoc.executeDynamicTestAsync(featureGherkin);
-                }
-                catch (e) {
-                    parseException = e;
-                }
-            });
-
-            then(`a parseException with the following description is thrown 
+                then(`a parseException with the following description is thrown 
             """
             Backgrounds only support using the given step definition. Consider moving the <display name> to a scenario.
             """`, () => {
-                    stepContext.docString.should.eql(parseException.description);
-                });
-        });
+                        stepContext.docString.should.eql(parseException.description);
+                    });
+            });
 
-    scenarioOutline(`Feature mixes BDD languages
+        scenarioOutline(`Feature mixes BDD languages
         Examples:
         | keyword  | suggestion |
         | it       | given      |
         | describe | scenario   |
         `, () => {
-            let featureGherkin: string;
+                let featureGherkin: string;
 
-            given(`the following feature
+                given(`the following feature
             """
             feature("Ensure bad Gherkin is not allowed", () => {
                 <keyword>("a <keyword> step definition is added to a feature", () => { });
             });            
             """`, () => {
-                    featureGherkin = stepContext.docString;
+                        featureGherkin = stepContext.docString;
+                    });
+
+                when(`executing feature`, async () => {
+                    try {
+                        await LiveDoc.executeDynamicTestAsync(featureGherkin);
+                    }
+                    catch (e) {
+                        parseException = e;
+                    }
                 });
 
-            when(`executing feature`, async () => {
-                try {
-                    await LiveDoc.executeDynamicTestAsync(featureGherkin);
-                }
-                catch (e) {
-                    parseException = e;
-                }
-            });
-
-            then(`a parseException with the following description is thrown 
+                then(`a parseException with the following description is thrown 
             """
             This Feature is using bdd syntax, did you mean to use <suggestion> instead?
             """`, () => {
-                    stepContext.docString.should.eql(parseException.description);
-                });
-        });
+                        stepContext.docString.should.eql(parseException.description);
+                    });
+            });
 
-    scenarioOutline(`Scenario mixes BDD languages
+        scenarioOutline(`Scenario mixes BDD languages
         Examples:
         | keyword  | suggestion |
       //| it       | given      |
         | describe | scenario   |
         `, () => {
-            let featureGherkin: string;
+                let featureGherkin: string;
 
-            given(`the following feature
+                given(`the following feature
             """
             feature("Ensure bad Gherkin is not allowed", () => {
                 scenario("Scenario uses bdd syntax", ()=> {
@@ -172,23 +172,23 @@ feature(`Ensure correctly structured Specs`, () => {
                 });
             });            
             """`, () => {
-                    featureGherkin = stepContext.docString;
+                        featureGherkin = stepContext.docString;
+                    });
+
+                when(`executing feature`, async () => {
+                    try {
+                        await LiveDoc.executeDynamicTestAsync(featureGherkin);
+                    }
+                    catch (e) {
+                        parseException = e;
+                    }
                 });
 
-            when(`executing feature`, async () => {
-                try {
-                    await LiveDoc.executeDynamicTestAsync(featureGherkin);
-                }
-                catch (e) {
-                    parseException = e;
-                }
-            });
-
-            then(`a parseException with the following description is thrown 
+                then(`a parseException with the following description is thrown 
             """
             This Scenario is using bdd syntax, did you mean to use <suggestion> instead?
             """`, () => {
-                    stepContext.docString.should.eql(parseException.description);
-                });
-        });
-});
+                        stepContext.docString.should.eql(parseException.description);
+                    });
+            });
+    });
